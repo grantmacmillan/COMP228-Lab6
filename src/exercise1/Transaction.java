20735a6704941659e0ddf2955fa64a26a5f6fedd
Ben Coombes <benzcoombes@gmail.com>
2021-12-12 17:17:17 -0500

    classes setup

---
+++ src/exercise1/Transaction.java
@@ -0,0 +1,30 @@
+package exercise1;
+
+public class Transaction implements Runnable{
+
+    public Account account;
+
+    public Transaction(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        try{
+            account.withdraw(200);
+            Thread.sleep(200);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+        account.deposit(350);
+        try{
+            Thread.sleep(1000);
+
+        }catch(InterruptedException e){
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Your new balance is: $" + account.getBalance());
+    }
+}
